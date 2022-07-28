c8a742423b3952606ef8478479c2f2a7c21c7cd2
Sandra Sanders <sandy-sanders@msn.com>
2022-07-28 13:35:38 -0700

    add test

---
+++ hello-circle/src/test/java/com/example/hellocircle/controller/HelloCircleControllerTest.java
@@ -0,0 +1,33 @@
+package com.example.hellocircle.controller;
+
+import org.junit.Test;
+import controller.HelloCircleController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloCircleController.class)
+@RunWith(SpringRunner.class)
+public class HelloCircleControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnAnswer() throws Exception {
+        // arrange and act
+        mockMvc.perform(get("/answer"))
+                .andDo(print())
+                .andExpect(status().isOk())        // assert
+                .andExpect(content().string("The answer is 42!"));}
+
+}
